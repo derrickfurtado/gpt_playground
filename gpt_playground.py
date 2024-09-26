b8416b909ff47e509ef75b07248e3829a9f8b4f5
Derrick Furtado <derrick.furtado10@gmail.com>
2024-09-26 09:57:01 -0600

    continued work on chat bot

--- gpt_playground.py
+++ gpt_playground.py
@@ -1,6 +1,4 @@
 import openai
-import config
-from openai import OpenAI
 from pdb import set_trace
 from key import gpt_api_key
 
@@ -9,19 +7,19 @@ openai.api_key = gpt_api_key
 
 
 
-def generate_text(prompt):
+def generate_text(prompt, max_tokens, temperature):
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
-        max_tokens=10,
-        temperature=0.7
+        max_tokens=max_tokens,                                  ### sets the limits for the number of tokens to be returned
+        temperature=temperature                                 ### sets the complexity of the returned response
     )
     return response.choices[0].message.content.strip()
 
 prompt = "Once upon a time"
 
 
-generated_text = generate_text(prompt)
+generated_text = generate_text(prompt, 50, 1)
 
 
 print(prompt, generated_text)
